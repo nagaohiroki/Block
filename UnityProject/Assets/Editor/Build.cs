1dfc984d2a43d70676c9d36398c4fd7f82124249
nagaohiroki <testplayers0226@gmail.com>
2019-01-07 23:53:40 +0900

    add travis setting

---
+++ UnityProject/Assets/Editor/Build.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEditor;
+using System.Collections.Generic;
+public static class Build
+{
+	[MenuItem("File/Build WebGL")]
+	public static void Run()
+	{
+		var sceneNames = new List<string>();
+		for(int i = 0; i < SceneManager.sceneCount; i++)
+		{
+			sceneNames.Add(SceneManager.GetSceneAt(i).path);
+		}
+		var option = new BuildPlayerOptions();
+		option.scenes = sceneNames.ToArray();
+		option.options = BuildOptions.None;
+		option.target = BuildTarget.WebGL;
+		option.locationPathName = "../docs";
+		BuildPipeline.BuildPlayer(option);
+	}
+}
