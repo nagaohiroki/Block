5ac9104cda5c4be6d77668da2514ea3d2ceafae3
nagaohiroki <testplayers0226@gmail.com>
2019-01-07 12:11:39 +0900

    change game

--- UnityProject/Assets/Block.cs
+++ UnityProject/Assets/Block.cs
@@ -8,21 +8,42 @@ public class Block : MonoBehaviour
 		Red,
 		Blue
 	}
-	public Vector2Int Pos{set; get;}
-	public Status CurrentStatus{set; get;}
-	static readonly Dictionary<Status, Color> mColorDict = new Dictionary<Status, Color>
+	readonly Dictionary<Status, Color> mColorDict = new Dictionary<Status, Color>
 	{
 		{Status.White, Color.white},
 		{Status.Red, Color.red},
 		{Status.Blue, Color.blue},
 	};
+	readonly List<Status> mSortPriority = new List<Status>
+	{
+		Status.White,
+		Status.Red,
+		Status.Blue,
+	};
+	public Vector2Int Pos{set; get;}
+	public Status CurrentStatus{set; get;}
 	public void UpdateStatus()
 	{
-		SetStatus(Status.Blue);
-	}
-	public void SetStatus(Status inStatus)
-	{
+		NextStatus();
 		var ren = GetComponent<Renderer>();
-		ren.material.color = mColorDict[inStatus];
+		ren.material.color = mColorDict[CurrentStatus];
+	}
+	void NextStatus()
+	{
+		int index = mSortPriority.IndexOf(CurrentStatus);
+		if(index == -1)
+		{
+			return;
+		}
+		int next = index + 1;
+		if(next >= mSortPriority.Count)
+		{
+			next = 0;
+		}
+		if(next < 0)
+		{
+			next = mSortPriority.Count - 1;
+		}
+		CurrentStatus = mSortPriority[next];
 	}
 }
--- UnityProject/Assets/Box.cs
+++ UnityProject/Assets/Box.cs
@@ -1,14 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 public class Box : MonoBehaviour
 {
 	[SerializeField]
 	Block mBlock;
+	[SerializeField]
+	Text mTelop;
+	[SerializeField]
+	Vector2Int mSize;
 	Dictionary<Vector2Int, Block> mBoxList;
-	Vector2Int mSize = new Vector2Int(8, 8);
-	void Start()
+	bool mIsClear;
+	readonly List<Vector2Int> mDirs = new List<Vector2Int>
 	{
+		Vector2Int.zero,
+		Vector2Int.down,
+		Vector2Int.up,
+		Vector2Int.left,
+		Vector2Int.right,
+	};
+	void Reset()
+	{
+		mIsClear = false;
+		if(mTelop != null)
+		{
+			mTelop.gameObject.SetActive(false);
+		}
 		mBoxList = new Dictionary<Vector2Int, Block>();
+		const float margin = 2.0f;
 		for(int y = 0; y < mSize.y; ++y)
 		{
 			for(int x = 0; x < mSize.x; ++x)
@@ -18,26 +37,27 @@ public class Box : MonoBehaviour
 				box.Pos = new Vector2Int(x, y);
 				box.transform.SetParent(transform);
 				var pos = box.transform.position;
-				pos.x = x * 2.0f;
-				pos.z = y * 2.0f;
+				pos.x = x * margin - ((mSize.x - 1) * margin * 0.5f);
+				pos.z = y * margin - ((mSize.y - 1) * margin * 0.5f);
 				box.transform.position = pos;
 				mBoxList.Add(box.Pos, box);
 			}
 		}
 	}
-
-	// Update is called once per frame
-	void Update()
+	bool CheckClear()
 	{
-		if(Input.GetButtonDown("Fire1"))
+		foreach(var item in mBoxList)
 		{
-			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if(Physics.Raycast(ray, out hit, Mathf.Infinity, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore))
+			if(item.Value == null)
 			{
-				ChangeStatus(hit.collider.gameObject.GetComponent<Block>());
+				continue;
+			}
+			if(item.Value.CurrentStatus != Block.Status.Blue)
+			{
+				return false;
 			}
 		}
+		return true;
 	}
 	void ChangeStatus(Block inBlock)
 	{
@@ -45,15 +65,7 @@ public class Box : MonoBehaviour
 		{
 			return;
 		}
-		inBlock.UpdateStatus();
-		var dirs = new List<Vector2Int>
-		{
-			Vector2Int.down,
-			Vector2Int.up,
-			Vector2Int.left,
-			Vector2Int.right,
-		};
-		foreach(var dir in dirs)
+		foreach(var dir in mDirs)
 		{
 			var pos = inBlock.Pos + dir;
 			if(!mBoxList.ContainsKey(pos))
@@ -63,4 +75,46 @@ public class Box : MonoBehaviour
 			mBoxList[pos].UpdateStatus();
 		}
 	}
+	void Start()
+	{
+		Reset();
+	}
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Reset();
+		}
+		if(mIsClear)
+		{
+			return;
+		}
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			//AutoPlayer();
+		}
+		if(Input.GetButtonDown("Fire1"))
+		{
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit, Mathf.Infinity, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore))
+			{
+				ChangeStatus(hit.collider.gameObject.GetComponent<Block>());
+			}
+			mIsClear = CheckClear();
+			if(mIsClear)
+			{
+				if(mTelop != null)
+				{
+					mTelop.gameObject.SetActive(true);
+				}
+			}
+		}
+	}
+
+	void AutoPlayer(Vector2Int inPos)
+	{
+		ChangeStatus(mBoxList[inPos]);
+	}
+
 }
